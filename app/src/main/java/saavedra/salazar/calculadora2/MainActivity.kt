5c8cf2ee3d94a8f100e34931d0f8ecd7c2e71885
josue033 <50113651+josue033@users.noreply.github.com>
2020-02-23 13:16:38 -0600

    calculadora mejorada

---
+++ app/src/main/java/saavedra/salazar/calculadora2/MainActivity.kt
@@ -0,0 +1,15 @@
+package saavedra.salazar.calculadora2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
